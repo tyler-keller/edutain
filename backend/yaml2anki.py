8bfa184d99a52469d3ce6bf30ebfa09db7666c02
Tyler Keller <tyler.c.keller@gmail.com>
2024-06-16 04:13:41 -0600

    update

--- backend/yaml2anki.py
+++ backend/yaml2anki.py
@@ -1,39 +1,57 @@
 import genanki
 import yaml
 import random
-import pprint
 
 def open_yaml_file(yaml_file):
     with open(yaml_file, 'r') as file:
         data = yaml.safe_load(file)
     return data
 
-def create_anki_deck(deck_name, deck_description):
+def get_random_id():
+    return int(''.join([str(random.randint(0, 9)) for _ in range(10)]))
+
+def create_anki_deck(deck_name):
     deck = genanki.Deck(
-            model_id=(''.join([random.randint(0, 9) for _ in range(30)])), 
+            deck_id=get_random_id(),
             name=deck_name, 
-            description=deck_description
         )
     return deck
 
-def create_anki_notes(anki_deck):
-    anki_deck.add_note(
-        genanki.Note(
-            model=anki_deck.model(),
-            fields=['What is the capital of France?', 'Paris']
-        )
-    )
+def add_anki_notes(anki_deck, flashcards):
+    qa_model = genanki.Model(
+        1607392319,
+        'Simple Model',
+        fields=[
+            {'name': 'Question'},
+            {'name': 'Answer'},
+        ],
+        templates=[
+            {
+            'name': 'Card',
+            'qfmt': '{{Question}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+            },
+    ])
 
+    for flashcard in flashcards:
+        question = flashcard['question']
+        answer = flashcard['answer']
+        anki_deck.add_note(
+            genanki.Note(
+                model=qa_model,
+                fields=[question, answer]
+            )
+        )
+    
 if __name__ == '__main__':
     data = open_yaml_file('output/deep-learning-anki/deep-learning-v1.yml')
-    # deck = create_anki_deck(data['title'], data['deck_description'])
-    # for datum in data:
-    #     print(datum)
-    pretty_data = pprint.pformat(data, compact=True)
-    print(pretty_data)
-    # print(data.get('title', 'No title found'))
-    # deck_name = data['deck_name']
-    # deck_description = data['deck_description']
-    # deck = create_anki_deck(deck_name, deck_description)
-    # create_anki_notes(deck)
-    # genanki.Package(deck).write_to_file('output/deep-learning-chapters-latex/deep-learning-chapters.apkg')
+    deck_name = data['title']
+    chapters = data['chapters']
+    chapter_decks = []
+    for chapter in chapters:
+        chapter_deck = create_anki_deck(f'{deck_name}' + '::' + f'Chapter-{chapter["chapter"]}')
+        add_anki_notes(chapter_deck, chapter['questions'])
+        chapter_decks.append(chapter_deck)
+    genanki.Package(chapter_decks).write_to_file('output/deep-learning-anki/deep-learning.apkg')
+
+# f"{subdeck}::{str(index + 1).zfill(2)} {deck}"
