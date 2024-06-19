3a5b1c41212a25a6448bae1d09a2ce4a87b29d6a
Tyler Keller <tyler.c.keller@gmail.com>
2024-06-18 22:24:23 -0600

    update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import { auth, user } from '$lib/firebase';
   import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+  import { navigate } from 'svelte-routing';
+  import { onMount } from 'svelte';
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
@@ -9,6 +11,35 @@
       console.log(result);
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  let files;
+
+  async function uploadFile() {
+    if (!files || files.length === 0) {
+      alert("No file selected!");
+      return;
+    }
+
+    const file = files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/upload', {
+        method: 'POST',
+        body: formData
+      });
+
+      if (response.ok) {
+        // Redirect to the progress page
+        navigate('/upload');
+      } else {
+        console.error('Failed to upload file:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error);
     }
   }
 </script>
@@ -22,16 +53,18 @@
   <h2 class="p-3 text-2xl font-bold text-black dark:text-white">Welcome</h2>
   <p class="text-lg w-3/4 text-center text-black dark:text-white">Edutain is a platform that allows you to learn *with the power of AI*.</p>
   <div class="p-3"></div>
+  <input accept=".epub" bind:files type="file" id="book" name="book" class='invisible'>
   <div class="p-6">
     {#if $user}
       <div class="flex flex-row justify-center space-x-8 sm:space-x-16">
-        <a href="/create-lesson">
-          <button class='font-bold rounded-md text-white p-4 bg-green-600'>Upload EPUB</button>
-        </a>
-        <button class="p-4 bg-red-500 text-white rounded-lg " on:click={() => signOut(auth)}>Sign out</button>
+        <label for="book" class="font-bold rounded-md text-white p-4 bg-green-600 cursor-pointer">
+          Upload EPUB
+        </label>
+        <button class='font-bold rounded-md text-white p-4 bg-green-600' on:click={uploadFile}> Upload EPUB</button>
+        <button class="font-bold p-4 bg-red-500 text-white rounded-lg" on:click={() => signOut(auth)}>Sign out</button>
       </div>
     {:else}
-      <a on:click={signInWithGoogle} href="/login">
+      <a on:click={signInWithGoogle} href="/">
         <button class='font-bold rounded-md text-white p-4 bg-blue-600'>Sign in with Google</button>
       </a>
     {/if}
